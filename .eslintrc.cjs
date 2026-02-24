0c94d3c247081329b770d911e7b654cbe3ff3ad4
ddalla0425 <hiwgod92@gmail.com>
2026-02-24 17:10:03 +0900

    chore: initial project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+}
---
+++ src/1_app/App.tsx
@@ -0,0 +1,7 @@
+import '@/1_app/styles/App.css'
+
+function App() {
+  return <h1>Vite + React</h1>
+}
+
+export default App
