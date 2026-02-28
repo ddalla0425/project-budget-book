b3c6542a003467de232cc002d79585f29c918657
ddalla0425 <hiwgod92@gmail.com>
2026-02-28 19:27:49 +0900

    add: 공용 Button 컴포넌트 작성 및 스타일 적용

---
+++ src/6_shared/ui/button/Button.tsx
@@ -0,0 +1,26 @@
+import * as S from './Button.style';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary' | 'danger';
+  size?: 'sm' | 'md' | 'lg';
+  fullWidth?: boolean;
+}
+
+export const Button = ({ 
+  variant = 'primary',
+  size = 'md', 
+  fullWidth = false,
+  children, 
+  ...props 
+}: ButtonProps) => {
+  return (
+    <S.Button 
+      $variant={variant} 
+      $size={size} 
+      $fullWidth={fullWidth}
+      {...props}
+    >
+      {children}
+    </S.Button>
+  );
+};
