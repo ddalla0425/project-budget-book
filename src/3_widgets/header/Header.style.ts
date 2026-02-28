2298117b9e2dec7016f8799667b6f73ea094b9fb
ddalla0425 <hiwgod92@gmail.com>
2026-02-28 19:27:22 +0900

    add: 헤더위젯 작성 및 스타일 적용

---
+++ src/3_widgets/header/Header.style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+    display: flex;
+    justify-content: space-between;
+    height: 80px;
+    padding: 10px 40px;
+    background-color: #F9F9F9;
+    
+     h1 {
+        display:flex;
+        align-items: center;
+        font-size: 1rem;
+    }
+    ul {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+`
