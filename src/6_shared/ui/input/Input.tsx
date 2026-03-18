260258c72e9578bb17c59ec68bf3f657eef065c9
ddalla0425 <hiwgod92@gmail.com>
2026-03-18 16:33:05 +0900

    update: 공용 인풋 숫자타입 일 때, 클릭시 이벤트 추가

--- src/6_shared/ui/input/Input.tsx
+++ src/6_shared/ui/input/Input.tsx
@@ -8,6 +8,23 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ variant = 'primary', deviceSize = 'md', id, ...props }, ref) => {
-    return <S.Input id={id} ref={ref} $variant={variant} $deviceSize={deviceSize} {...props} />;
+    const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+      // 숫자 타입일 때 클릭하면 텍스트 전체 선택
+      if (props.type === 'number') {
+        if (Number(e.target.value) === 0) {
+          e.target.select();
+          // React의 onChange 이벤트를 수동으로 트리거하는 가장 안전한 방법
+          const event = {
+            ...e,
+            target: { ...e.target, name: props.name, value: '' },
+          } as unknown as React.ChangeEvent<HTMLInputElement>;
+
+          props.onChange?.(event);
+        }
+      }
+      // 기존에 전달받은 onFocus가 있다면 실행
+      props.onFocus?.(e);
+    };
+    return <S.Input id={id} ref={ref} $variant={variant} $deviceSize={deviceSize} {...props} onFocus={handleFocus} />;
   }
 );
