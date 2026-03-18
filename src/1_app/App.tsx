be655b376305acdcb101cd7de5a68689659bd752
ddalla0425 <hiwgod92@gmail.com>
2026-03-18 16:39:31 +0900

    feat: 앱단에서 금융기관 테이블 조회후 스토어 저장로직 추가

--- src/1_app/App.tsx
+++ src/1_app/App.tsx
@@ -1,9 +1,21 @@
 import { GlobalStyle } from '@/1_app/styles';
 import { AppRouter } from '@/1_app/providers';
 import { Modal } from '@/6_shared/ui/modal';
+import { useGetFinancialInstitutionsQuery, useInstitutionStore } from '@/5_entities/institution';
 import 'swiper/bundle';
+import { useEffect } from 'react';
 
 function App() {
+  const { data: institutions } = useGetFinancialInstitutionsQuery();
+  const setInstitutions = useInstitutionStore((s) => s.setInstitutions);
+
+  useEffect(() => {
+    if (institutions) {
+      setInstitutions(institutions);
+      console.log('금융기관 정보가 스토어에 저장되었습니다.');
+    }
+  }, [institutions, setInstitutions]);
+
   return (
     <>
       <GlobalStyle />
